8f41814fa71bf42f66e5af9c857651ebcb879350
Esteban Mallen <esteban.mallen@appdirect.com>
2017-05-11 16:28:07 -0300

    Maven and SpringBoot skeleton

---
+++ src/main/java/com/appdirect/healthmonitor/HealthMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.appdirect.healthmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthMonitorApplication.class, args);
+	}
+}
